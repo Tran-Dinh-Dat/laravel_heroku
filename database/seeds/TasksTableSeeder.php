cb15207f6ef819143da3ed8a219294b6bb1a279c
--add <=>
2019-09-16 17:46:50 +0700

    add seeder task

---
+++ database/seeds/TasksTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use App\Task;
+class TasksTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+        for ($i=0; $i < 10; $i++) { 
+            Task::create([
+                'name' => $faker->name,
+            ]);
+        }
+    }
+}
